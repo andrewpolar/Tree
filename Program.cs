c1c6d4331b60b613a2079f5a2806c2017dbfe722
andrewpolar <61638749+andrewpolar@users.noreply.github.com>
2021-04-17 00:20:56 -0400

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Runtime.Serialization;
 using System.Runtime.Serialization.Formatters.Binary;
@@ -21,17 +21,17 @@ namespace Tree
 
             Console.WriteLine("\nModelling of physical system");
             dh.BuildAirfoilData();
-            TreeModel tm2 = new TreeModel(7, 4, 2, dh);
+            TreeModel tm2 = new TreeModel(5, 3, 5, dh);
             tm2.ExecuteMainFlow(100, mu);
 
             Console.WriteLine("\nModelling of biological system");
             dh.BuildMClassData();
-            TreeModel tm3 = new TreeModel(3, 8, 5, dh);
+            TreeModel tm3 = new TreeModel(3, 10, 5, dh);
             tm3.ExecuteMainFlowQuantized(100, mu);
 
             Console.WriteLine("\nModelling of social system");
             dh.BuildBankChurnData();
-            TreeModel tm4 = new TreeModel(3, 7, 4, dh);
+            TreeModel tm4 = new TreeModel(3, 6, 10, dh);
             tm4.ExecuteMainFlowQuantized(100, mu);
         }
     }
